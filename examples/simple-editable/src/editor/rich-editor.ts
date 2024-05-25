084075529da7bbd3ac10497e6249690a74e5aa6c
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2024-05-25 13:47:31 +0300

    test: set up example

---
+++ examples/simple-editable/src/editor/rich-editor.ts
@@ -0,0 +1,32 @@
+import { RichEditable } from 'editable'
+
+const editor = document.getElementById('rich-editor')
+let editorLines: Array<RichEditable> = []
+
+function addLine(editable: RichEditable) {
+  editorLines.push(editable)
+  editable.addKeyListener({ key: 'Enter', ctrl: true }, () =>
+    addLine(new RichEditable({ newLine: 'basic' }))
+  )
+  editable.setOnDeleteWhenEmpty(key => {
+    const currentIndex = editorLines.findIndex(line => line === editable)
+    const backspace = key === 'Backspace' && currentIndex > 0
+    const del =
+      key === 'Delete' &&
+      currentIndex >= 0 &&
+      currentIndex < editorLines.length - 1
+
+    if ((backspace || del) && editorLines.length > 1) {
+      editor?.removeChild(editable.Element)
+      editorLines = [...editorLines.filter(line => line !== editable)]
+
+      editorLines.at(backspace ? currentIndex - 1 : currentIndex)?.focus('end')
+    }
+  })
+
+  editor?.appendChild(editable.Element)
+  editable.Element.focus()
+}
+
+export const setup = () =>
+  addLine(new RichEditable({ newLine: 'basic', placeholder: 'First Line' }))
