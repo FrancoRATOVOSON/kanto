f050aa718448884cee8a1c78aa5202f65005d684
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2024-05-26 20:07:06 +0300

    feat: add css style for inline styles

--- packages/editable/src/rich-editable/utils/const.ts
+++ packages/editable/src/rich-editable/utils/const.ts
@@ -1,7 +1,7 @@
 export const FONTWEIGHT_ATTRIBUTE = 'data-font-weight'
 export const FONTSTYLE_ATTRIBUTE = 'data-font-style'
-export const INLINECODE_ATTRIBUTE = 'data-inline-code'
 export const UNDERLINED_ATTRIBUTE = 'data-text-underlined'
 export const LINETHROUGH_ATTRIBUTE = 'data-text-linethrough'
+export const INLINECODE_ATTRIBUTE = 'data-inline-code'
 
 export const INLINE_STYLE_ACTION = ['bold', 'italic', 'underlined', 'linethrough', 'code'] as const
